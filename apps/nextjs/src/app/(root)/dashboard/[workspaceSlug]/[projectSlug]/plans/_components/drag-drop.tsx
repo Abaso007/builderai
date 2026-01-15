f560fbd74c733a87bc3e26cacaf07be4e54eb808
Sebastian Franco <jhonsfran@gmail.com>
2026-01-15 18:26:57 +0100

    fix: format

--- apps/nextjs/src/app/(root)/dashboard/[workspaceSlug]/[projectSlug]/plans/_components/drag-drop.tsx
+++ apps/nextjs/src/app/(root)/dashboard/[workspaceSlug]/[projectSlug]/plans/_components/drag-drop.tsx
@@ -75,11 +75,22 @@ export default function DragDrop({ children }: { children: React.ReactNode }) {
                   ...feature,
                   ...planVersionFeature,
                   metadata: {
-                    realtime: planVersionFeature.metadata?.realtime ?? feature.metadata?.realtime ?? false,
-                    notifyUsageThreshold: planVersionFeature.metadata?.notifyUsageThreshold ?? feature.metadata?.notifyUsageThreshold ?? 95,
-                    overageStrategy: planVersionFeature.metadata?.overageStrategy ?? feature.metadata?.overageStrategy ?? "none",
-                    blockCustomer: planVersionFeature.metadata?.blockCustomer ?? feature.metadata?.blockCustomer ?? false,
-                    hidden: planVersionFeature.metadata?.hidden ?? feature.metadata?.hidden ?? false,
+                    realtime:
+                      planVersionFeature.metadata?.realtime ?? feature.metadata?.realtime ?? false,
+                    notifyUsageThreshold:
+                      planVersionFeature.metadata?.notifyUsageThreshold ??
+                      feature.metadata?.notifyUsageThreshold ??
+                      95,
+                    overageStrategy:
+                      planVersionFeature.metadata?.overageStrategy ??
+                      feature.metadata?.overageStrategy ??
+                      "none",
+                    blockCustomer:
+                      planVersionFeature.metadata?.blockCustomer ??
+                      feature.metadata?.blockCustomer ??
+                      false,
+                    hidden:
+                      planVersionFeature.metadata?.hidden ?? feature.metadata?.hidden ?? false,
                   },
                 }
               : feature
