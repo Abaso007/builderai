047a7c126cf11040b81e0fc64cbe18e49bb91ba4
Sebastian Franco <jhonsfran@gmail.com>
2026-03-13 07:10:59 +0100

    chore: refactoring billing

--- internal/db/src/schema/billingPeriods.ts
+++ internal/db/src/schema/billingPeriods.ts
@@ -13,7 +13,6 @@ import { pgTableProject } from "../utils/_table"
 import { cuid, timestamps } from "../utils/fields"
 import { projectID } from "../utils/sql"
 import { customers } from "./customers"
-import { grants } from "./entitlements"
 import { billingPeriodStatusEnum, billingPeriodTypeEnum, whenToBillEnum } from "./enums"
 import { invoices } from "./invoices"
 import { projects } from "./projects"
@@ -28,8 +27,6 @@ export const billingPeriods = pgTableProject(
     subscriptionId: cuid("subscription_id").notNull(),
     // customer id is the id of the customer that is associated with the billing period
     customerId: cuid("customer_id").notNull(),
-    // grant id is the id of the grant that is applied to the billing period
-    grantId: cuid("grant_id").notNull(),
     subscriptionPhaseId: cuid("subscription_phase_id").notNull(),
     subscriptionItemId: cuid("subscription_item_id").notNull(),
     status: billingPeriodStatusEnum("status").notNull().default("pending"),
@@ -71,11 +68,6 @@ export const billingPeriods = pgTableProject(
       columns: [table.customerId, table.projectId],
       foreignColumns: [customers.id, customers.projectId],
       name: "billing_periods_customer_id_fkey",
-    }).onDelete("cascade"),
-    grantfk: foreignKey({
-      columns: [table.grantId, table.projectId],
-      foreignColumns: [grants.id, grants.projectId],
-      name: "billing_periods_grant_id_fkey",
     }).onDelete("cascade"),
     // project fk
     projectfk: foreignKey({
@@ -122,10 +114,6 @@ export const billingPeriodRelations = relations(billingPeriods, ({ one }) => ({
   project: one(projects, {
     fields: [billingPeriods.projectId],
     references: [projects.id],
-  }),
-  grant: one(grants, {
-    fields: [billingPeriods.grantId, billingPeriods.projectId],
-    references: [grants.id, grants.projectId],
   }),
   customer: one(customers, {
     fields: [billingPeriods.customerId, billingPeriods.projectId],
